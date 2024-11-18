d789ac2b3691790083219795aca4a15ad4a72dc7
besthong <jhjh30111@naver.com>
2024-11-18 16:22:08 +0900

    chore: 라우터 등록 예시 추가

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 
 # 각 도메인의 라우터를 등록 예시
 # app.include_router(my_page_router.router, prefix="/api/mypage", tags=["mypage"])
-app.include_router(apartments_router, prefix="/api", tags=["apartments"])
+# app.include_router(apartments_router, prefix="/api", tags=["apartments"])
 
 
 @app.get("/")
