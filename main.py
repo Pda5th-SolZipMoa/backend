1ec4b48f08b67d0d9631f6e5078db92659ead886
besthong <jhjh30111@naver.com>
2024-11-18 16:21:15 +0900

    chore: requirements.txt 추가

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# main.py
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
+from domain.apartments.getApartments import router as apartments_router
+import requests
+import os
+
+from pydantic import BaseModel
 app = FastAPI()
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://3.36.99.137"],  # 허용할 도메인 (프론트엔드 주소)
+    allow_origins=["http://localhost:5173"],  # 허용할 도메인 (프론트엔드 주소)
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 HTTP 헤더 허용
@@ -16,7 +19,7 @@ app.add_middleware(
 
 # 각 도메인의 라우터를 등록 예시
 # app.include_router(my_page_router.router, prefix="/api/mypage", tags=["mypage"])
-
+app.include_router(apartments_router, prefix="/api", tags=["apartments"])
 
 
 @app.get("/")
